d148cddc15cd1d47a4819807b6bb4a16132c07ed
meenalk44 <meenalskulkarni@gmail.com>
2017-03-10 10:56:23 -0700

    teacher panel 10/3

--- MyApp/routes/classController.js
+++ MyApp/routes/classController.js
@@ -29,75 +29,11 @@ module.exports.classSettings = function(req,res){
 	console.log("in classSettings");
 	var id = req.param('id');
 	console.log(id);
-	res.render('classSettings',{id:id});
+	var entries=[];
+	res.render('classSettings',{entries : JSON.stringify(entries),id:id});
 	//res.render('success',{msg:'',redirect:'/'});
 	
 };
-
-
-
-/*module.exports.addStudents = function(req,res){
-	var class_id = req.param('id');
-	var stud_id_arr =[];
-	var emails = req.body.stud_id;
-	var email_arr = emails.split(",");
-	var promise_arr = [];
-	
-	
-	email_arr.forEach(function(email){
-		var p = new Promise(function (resolve, reject){
-			var newUser = new User({
-				email: email,
-				role: 'Student'
-			});
-			console.log(newUser);
-			newUser.save(function(err,entry){
-				if(err){
-					console.log("Error while posting student to db");
-					reject(err);
-				}else{
-					console.log("student saved to db:  "+entry);
-					resolve(entry.id);
-					var newClassStudents = new Class();
-					newClassStudents.student_ids.push(entry.id);
-					
-				}
-			});
-			
-		});
-		promise_arr.push(p);
-		
-	});
-	
-	console.log("Promise: "+promise_arr);
-	
-	Promise.all(promise_arr)
-		.then(function (stud_id) {
-			// update student_ids in class object
-			console.log("-----++++ "+ stud_id);
-			stud_id_arr.push(stud_id);
-			console.log("***** "+stud_id_arr);
-			
-		}).then(function(stud_id_arr){
-			Class.find({}).exec(function(err, entries){
-				if(err)
-					console.log(err)
-				else{
-					console.log("-------");
-					console.log(entries);
-					console.log("-------");
-					
-				}
-				});
-			
-		});
-	
-};*/
-
-
-
-
-
 
 
 module.exports.addStudents = function(req,res){
@@ -137,33 +73,30 @@ module.exports.addStudents = function(req,res){
 		.then(function (stud_id) {
 			// update student_ids in class object
 			console.log("-----++++ "+ stud_id);
-			stud_id_arr.push(stud_id);
-			console.log("***** "+stud_id_arr);
-			
-		}).then(function(stud_id_arr){
-			var newClassStudents = new Class({
-				student_ids	:	stud_id_arr
-			});
-			console.log("##### "+newClassStudents);
-			Class.findOneAndUpdate({'id':id},newClassStudents,{new:true},function(err,entries){
+			//stud_id_arr.push(stud_id);
+			//console.log("***** "+stud_id_arr);
+			Class.findByIdAndUpdate(id, {$set:{student_ids : stud_id}},{new:true}, function(err,docs){
 				if(err){
 					console.log("Could not find classes");
 					console.log(err);
 				}
 				else{
 					console.log("addedToClass");
-					console.log("saved students to class"+entries);
-					//res.render('classSettings',{entries:JSON.stringify(entries),id:id});
+					//res.render('classSettings',{entries:JSON.stringify(docs),id:id});
+					Class.find({})
+						.populate('student_ids')
+						.exec(function(err,docs){
+							console.log(docs);
+							res.render('classSettings',{entries:JSON.stringify(docs),id:id});
+						});
 				}
-			});
 		});
-			
-			
+	});
 	
 	
 	
 	
-	Class.find({}).exec(function(err, entries){
+	/*Class.find({}).exec(function(err, entries){
 		if(err)
 			console.log(err)
 		else{
@@ -172,7 +105,7 @@ module.exports.addStudents = function(req,res){
 			console.log("-------");
 			
 		}
-		});
+		});*/
 	
 };	
 	
@@ -190,40 +123,5 @@ module.exports.classDetails = function(req,res){
 			res.render('classes',{entries:JSON.stringify(entries)});
 		}
 	});
-//	Class.findById(teacher_id,function(err,entries){
-//		if(err)
-//			console.log("Could not find classes");
-//		else
-//			console.log(entries);
-//			
-//			
-//	});	
-	//var count = 0;
-	/*Class.count('student_ids')
-	.where('teacher_id').equals(teacherId).exec(function(err,cnt){
-		if(err)
-			console.log(err);
-		else{
-			count = cnt;
-			console.log("*** "+cnt);
-			//res.render('classes',{entries:JSON.stringify(entries), count:cnt});
-		}
-	});*/
-	
-	/*Class.aggregate([
-	                 {$project	:	{
-	                	 item	:	1,
-	                	 count	:	{	$size	:	"$student_ids"}
-	                 }}
-	]);
-	console.log("***## "+count);
-	Class.find({}).exec(function(err, entries){
-		if(err)
-			console.log(err)
-		else{
-			
-			res.render('classes',{entries:JSON.stringify(entries), count:count});
-		}
-	});*/
-	
+
 };
