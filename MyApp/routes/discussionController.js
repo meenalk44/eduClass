5a2c5a5821cab9a35ae44c067fd5d955a8ca7242
meenalk44 <meenalskulkarni@gmail.com>
2017-03-24 11:23:12 -0700

    render discussion based on template

--- MyApp/routes/discussionController.js
+++ MyApp/routes/discussionController.js
@@ -4,6 +4,7 @@ var db = mongoose.connection;
 var Class = require('../models/classSchema');
 var Question = require('../models/questionSchema');
 var Answer = require('../models/answerSchema');
+//var Reply = require('../models/replySchema');
 
 module.exports.dicussionShow = function(req, res){
 	var discussion_id = req.param('id');
@@ -16,19 +17,88 @@ module.exports.dicussionShow = function(req, res){
         else {
             if (classInfo.template != 'Nested') {
                 current_template = 'Flat';
-            } else current_template = 'Nested';
+                console.log("___!!! "+current_template);
+            } else{
+                current_template = 'Nested';
+                console.log("*** !!! "+current_template);
+            }
         }
     });
-	Question.find({'discussion_id':discussion_id}).sort({'_id':-1})
-		.populate('answers_level1').exec(function(err,posts){
-		if(err)
-			console.log(err);
-		else{
-			console.log("Posts: "+posts);
-			res.render('discussion',{entries : JSON.stringify(posts),discussion_id:discussion_id, class_id:class_id,template:current_template});
-		}
-	
-	});
+    if(current_template === 'Flat'){
+        /*Question.find({'discussion_id':discussion_id}).sort({'_id':-1})
+            .populate('answers_level1').exec(function(err,posts){
+            if(err)
+                console.log(err);
+            else{
+                console.log("Posts: "+posts);
+                res.render('discussion',{entries : JSON.stringify(posts),discussion_id:discussion_id, class_id:class_id,template:current_template});
+            }
+
+        });*/
+        Answer.find({'discussion_id':discussion_id}).sort({'_id':-1})
+            .populate([
+                {
+                    path    :   'que_id',
+                    model   :   'Question',
+                    populate:{
+                        path:'question',
+                        model:'Question'
+                    }
+                },
+                {
+                    path    :   'answers_level1',
+                    model   :   'Answer',
+                    populate:{
+                        path:'question',
+                        model:'Question',
+                        populate:{
+                            path:'answer',
+                            model:'Answer'
+                        }
+                    }
+                }
+            ]).exec(function (err,posts) {
+                if(err)
+                    console.log(err);
+                else{
+                    console.log("***Flat Posts: "+posts);
+                    res.send(posts);
+
+                }
+
+        });
+
+    }else{
+        Answer.find({'discussion_id':discussion_id}).sort({'_id':-1})
+            .populate([
+                {
+                    path    :   'que_id',
+                    model   :   'Question',
+                    populate:{
+                        path:'question',
+                        model:'Question'
+                    }
+                },
+                {
+                    path    :   'replies',
+                    model   :   'Answer',
+                    populate:{
+                        path:'answer',
+                        model:'Answer'
+                    }
+                }
+            ]).exec(function (err,posts) {
+                if(err)
+                    console.log(err);
+                else {
+                    console.log("Nested Posts:"+posts);
+                    //res.send(posts);
+                    res.render('discussion',{entries : JSON.stringify(posts),discussion_id:discussion_id, class_id:class_id,template:current_template});
+                }
+
+        });
+    }
+
 	
 };		
 
@@ -91,7 +161,7 @@ module.exports.postAns = function (req,res) {
             if (err)
                 console.log(err);
             else {
-                if (current_template == 'Flat') {
+               // if (current_template == 'Flat') {
 
                     Question.findByIdAndUpdate(ques_id, {$push:{answers_level1:ansEntry._id}}, function (err, que) {
                         if (err)
@@ -102,7 +172,7 @@ module.exports.postAns = function (req,res) {
                             res.redirect('/classes/'+class_id+'/discussion/'+discussion_id);
                         }
                     });
-                }
+              //  }
 
             }
         });
@@ -119,24 +189,34 @@ module.exports.postReply = function (req,res) {
     var ans_id = req.param('ans_id');
     var current_template = '';
     console.log("Reply: "+ ans_id);
-    var newAns = new Answer({
-        discussion_id: discussion_id,
-        que_id: ques_id,
+    /*var newReply = new Reply({
+        //discussion_id: discussion_id,
+        //que_id: ques_id,
+        ans_id: ans_id,
         user_id: req.user.id,
         fullname: req.user.fullname,
         profile_img: req.user.profile_img,
-        ans_body: req.body.ansBody,
-        ans_level: 1,
+        reply_body: req.body.replyBody,
+        timeStamp: new Date()
+    });*/
+    var newReply = new Answer({
+        discussion_id: discussion_id,
+        que_id: ques_id,
+        //ans_id: ans_id,
+        user_id: req.user.id,
+        fullname: req.user.fullname,
+        profile_img: req.user.profile_img,
+        ans_body: req.body.replyBody,
         timeStamp: new Date()
     });
-    newAns.save(function (err, ansEntry) {
+    newReply.save(function (err, replyEntry) {
         if (err)
             console.log(err);
         else {
-            console.log("Reply entry: "+ansEntry);
+            console.log("Reply entry: "+replyEntry);
             //Answer.findByIdAndUpdate(ans_id, {$push:{replies:ansEntry._id}}, function (err, updAns) {
             Answer.update({'_id':ans_id},
-                {$push:{'replies':{$each:ansEntry._id}}},function(err,updAns){
+                {$push:{'replies':replyEntry._id}},function(err,updAns){
                 if (err)
                     console.log(err);
                 else {
