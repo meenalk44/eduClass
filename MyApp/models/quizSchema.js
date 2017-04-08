fd672135ba93a0a56d7b5473a5f0cc974129eef7
meenalk44 <meenalskulkarni@gmail.com>
2017-04-08 10:58:20 -0700

    create a quiz

--- MyApp/models/quizSchema.js
+++ MyApp/models/quizSchema.js
@@ -1,15 +1,15 @@
 var mongoose = require('mongoose');
 var quizSchema = new mongoose.Schema({
-	subject : String,
-	question: [String],
-	timestamp: Date,
-	user_id : String,
-	username : String,
-	answers : [{ans : String,
-		user_id : String,
-		username : String,
-		anstimestamp: Date
-	}]
+	class_id 	: {type: mongoose.Schema.Types.ObjectId, ref : 'Class'},
+	questions	: [{
+        que_num: Number,
+        que_body: String
+    }],
+	timestamp	: String,
+	user_id 	: {type: mongoose.Schema.Types.ObjectId, ref : 'User'},
+    fullname 	: {type: mongoose.Schema.Types.String, ref : 'User'},
+    profile_img	: {type: mongoose.Schema.Types.String, ref : 'User'}
+
 	
 });
 
--- MyApp/routes/quizController.js
+++ MyApp/routes/quizController.js
@@ -2,40 +2,207 @@ var mongoose = require('mongoose');
 var dbconn = require('../helpers/dbconn');
 var db = mongoose.connection;
 var Quiz = require('../models/quizSchema');
+var async = require('async');
+var Promise = require('bluebird');
+var User = require('../models/userSchema');
 
 
-
-module.exports.quizShow = function(req, res){
-  res.render('quiz');
+module.exports.quizSettings = function(req, res){
+	var class_id = req.param('class_id');
+  	res.render('quiz',{'class_id':class_id});
 };
 
-module.exports.quizCreate = function(req,res){
-	var newQue = new Quiz({
-		subject: 'Subject_1',
-		question : req.body.que1,
-		quetimestamp: new Date(),
-		user_id : 'userid',
-		username : 'UserName',
-	});
-	console.log(newQue);
-	newQue.save(function(err,entryQue){
+/*
+module.exports.createQuiz = function(req,res){
+	console.log("Inside quiz controller--------\n");
+    var class_id = req.param('class_id');
+    console.log(class_id);
+	var numOfQues = Object.keys(req.body).length;
+	var time = new Date();
+	time = time.toDateString();
+    Quiz.update({'id': '58e7b96c38bcd90eecd08da8'}, {
+            $push: {
+                'questions': {
+                    $each: [
+                        {
+                            'que_num': 222,
+                            'que_body': 'Update ho ja yaar'
+                        }
+                    ]
+                }
+            }
+        },
+        function(err, entries) {
+            if (err) {
+                res.send("Error");
+            } else {
+                console.log("****"+ JSON.stringify(entries));
+                res.send(entries);
+
+
+            }
+
+            // res.render('showQuiz', {entries: JSON.stringify(entries)});
+        });
+    /*var newQue = new Quiz({
+        class_id: class_id,
+        questions:{
+            que_num : 1,
+            que_body : req.body.textbox1 ,
+
+        },
+        timestamp: time,
+        user_id : req.user.id,
+        fullname : req.user.fullname,
+        profile_img : req.user.profile_img
+    });*/
+	//var cnt = 1;
+
+	//console.log(req.param('msg'));
+
+        /*var textboxNum = "textbox"+cnt;
+        console.log(textboxNum);
+
+
+    }*/
+	/*console.log(newQue);
+    Quiz.update({'id': '58e7b96c38bcd90eecd08da8'}, {
+            $push: {
+                'questions': {
+                    $each: [
+                        {
+                            'que_num': 222,
+                            'que_body': 'req.body.textboxNum'
+                        }
+                    ]
+                }
+            }
+        },
+        function(err, entries) {
+            if (err) {
+                res.send("Error");
+            } else {
+                console.log("****"+ JSON.stringify(entries));
+                res.send(entries);
+
+
+            }
+
+            // res.render('showQuiz', {entries: JSON.stringify(entries)});
+        });*/
+	/*newQue.save(function(err,quiz){
 		if(err){
 			return console.log("Error while posting question to db");
 		}else{
-			console.log("Question saved to db:  "+entryQue);
-			Quiz.find({}).exec(function(err,entries){
-				if(err){
-					res.send("Error");
-				}else{
-					console.log(entries);
-				}
-				res.render('showQuiz',{entries : JSON.stringify(entries)});	
-			});
+
+			//var textboxNum = "textbox"+cnt;
+			console.log("____ "+ req.body.textboxNum);
+			console.log("Question saved to db:  "+quiz.id);
+            //for(var cnt =1;cnt<=numOfQues; cnt++) {
+			/!*async.each(numOfQues,function (cnt,callback) {
+				console.log("888 "+cnt+"   "+index);
+				callback();
+
+            },function (err) {
+				if(err)
+					console.log(err);
+				else {
+                    console.log("end");
+                }
+
+            });*!/
+                //textboxNum = "textbox"+cnt;
+
+
+           // }
+
 		}
 	});
-		
-};
 
+};
+*/
+
+
+module.exports.createQuiz = function (req,res) {
+
+    var class_id = req.param('class_id');
+    var ques_arr = [];
+    var numOfQues = Object.keys(req.body).length;
+    var time = new Date();
+    time = time.toDateString();
+    var newQue ={};
+    //var pro = new Promise(function(resolve,reject{
+    var cnt = 0;
+    async.forEach(Object.keys(req.body), function (prop,callback) {
+        if (req.body.hasOwnProperty(prop)) {
+
+            newQue ={
+                que_num : cnt,
+                que_body: req.body[prop]
+            };
+            ques_arr[cnt] = newQue;
+            cnt = cnt + 1;
+        }
+        console.log("--- "+ JSON.stringify(newQue));
+        callback();
+    },function (err) {
+        if(err)
+            res.send("ERR in ASYNC"+err);
+        else{
+            console.log("--ASYNC-- "+ JSON.stringify(ques_arr));
+            var newQuiz = new Quiz({
+                 class_id: class_id,
+                 timestamp: time,
+                 user_id: req.user.id,
+                 fullname: req.user.fullname,
+                 profile_img : req.user.profile_img
+                 //questions : newQue
+
+             });
+            console.log("**** "+ newQuiz);
+            newQuiz.save(function(err, quiz){
+                 if(err)
+                    console.log(err);
+                 else{
+                    //res.send(quiz);
+                     async.each(ques_arr,function (queObj, cb) {
+                         Quiz.update({'id':quiz.id},{$push:{'questions':{$each: queObj}}},
+                             function (err,savedResp) {
+                                if(err)
+                                    res.send("ERR in Update"+err);
+                                else{
+                                    console.log("AFter Update  :  "+JSON.stringify(savedResp));
+                                }
+
+                             });
+                         cb();
+
+
+                     },
+                     function (err) {
+                         console.log("Q    "+quiz);
+
+                     });
+                     //console.log("Quiz: "+quiz);
+                 }
+             });
+        }
+
+    });
+    /*for (var property in req.body) {
+        if (req.body.hasOwnProperty(property)) {
+            var cnt = 1;
+            var newQue ={
+                    que_num : cnt,
+                    que_body: req.body[property]
+            };
+            cnt++;
+        }
+        console.log("---- "+newQue);
+    }*/
+
+
+};
 
 module.exports.storeAns = function(req,res){
 	console.log("---------*****"+JSON.stringify(req.body));
